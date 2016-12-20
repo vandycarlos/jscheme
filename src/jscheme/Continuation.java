ccfce20fb2f842df72c687f41b12c5c0a0eae64b
maple <maple.hl@gmail.com>
2016-12-20 16:34:59 +0800

    code format

--- src/jscheme/Continuation.java
+++ src/jscheme/Continuation.java
@@ -1,21 +1,20 @@
 package jscheme;
 
 /**
- * @author Peter Norvig, peter@norvig.com http://www.norvig.com
- *         Copyright 1998 Peter Norvig, see http://www.norvig.com/license.html
+ * @author Peter Norvig, peter@norvig.com http://www.norvig.com Copyright 1998 Peter Norvig, see
+ *     http://www.norvig.com/license.html
  */
-
 public class Continuation extends Procedure {
 
-    RuntimeException cc = null;
-    public Object value = null;
+  RuntimeException cc = null;
+  public Object value = null;
 
-    public Continuation(RuntimeException cc) {
-        this.cc = cc;
-    }
+  public Continuation(RuntimeException cc) {
+    this.cc = cc;
+  }
 
-    public Object apply(Scheme interpreter, Object args) {
-        value = first(args);
-        throw cc;
-    }
+  public Object apply(Scheme interpreter, Object args) {
+    value = first(args);
+    throw cc;
+  }
 }
